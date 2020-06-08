da47c0d37e19db6bdc0e05663aad1401aa9f5222
divyamakkar26 <divyamakkar24@gmail.com>
2020-06-08 17:22:57 +0530

    test

--- app/services/tests/iTunesApi.test.js
+++ app/services/tests/iTunesApi.test.js
@@ -1,19 +1,19 @@
-// import MockAdapter from 'axios-mock-adapter';
-// import { getApiClient } from '@utils/apiUtils';
-// import { getSongsData } from '../iTunesApi';
+import MockAdapter from 'axios-mock-adapter';
+import { getApiClient } from '@utils/apiUtils';
+import { getSongsData } from '../iTunesApi';
 
 describe('iTunesApi tests', () => {
-  // sconst songName = 'hey';
-  //   it('should make the api call to "/search?term="', async () => {
-  //     const mock = new MockAdapter(getApiClient().axiosInstance);
-  //     const data = [
-  //       {
-  //         resultCount: 1,
-  //         results: [{ songName }]
-  //       }
-  //     ];
-  //     mock.onGet(`/search?term=${songName}`).reply(200, data);
-  //     const res = await getSongsData(songName);
-  //     expect(res.data).toEqual(data);
-  //   });
+  const songName = 'mac';
+  it('should make the api call to "/search?term="', async () => {
+    const mock = new MockAdapter(getApiClient('itunes').axiosInstance);
+    const data = [
+      {
+        resultCount: 1,
+        results: [{ songName }]
+      }
+    ];
+    mock.onGet(`/search?term=${songName}`).reply(200, data);
+    const res = await getSongsData(songName);
+    expect(res.data).toEqual(data);
+  });
 });
